b2e9f8c5000c9066f7f619ef1563880681756523
riyo-y <riyo19900307@gmail.com>
2021-04-28 01:41:25 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,14 +9,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+
+
   def create
-    @item = Items.new(item_params)
+    
+    @item = Item.new(item_params)
     if @item.save
       redirect_to  root_path
     else
       render:new
     end
   end
+  
+
 
   def show
   @item = item.find(params[:id])
@@ -24,6 +29,6 @@ class ItemsController < ApplicationController
 
   private
    def item_params
-    params.require(:item).permit(:products_name, :price, :user, :category_id, :products_states_id, :shipping_id, :admins_information_id, :arrival_date_id, :comments, :image).marge(user_id: current_user.id)
+    params.require(:item).permit(:products_name, :price, :user, :category_id, :products_states_id, :shipping_id, :admins_information_id, :arrival_date_id, :comments, :image).merge(user_id: current_user.id)
   end
 end
--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,11 +1,7 @@
 window.addEventListener('load', () => {
-  console.log("OK");
   const priceInput = document.getElementById("item-price");
-console.log(priceInput);
 priceInput.addEventListener("input", () => {
-  console.log("イベント発火");
   const inputValue = priceInput.value;
-   console.log(inputValue);
    const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
     const addprofitDom = document.getElementById("profit");
